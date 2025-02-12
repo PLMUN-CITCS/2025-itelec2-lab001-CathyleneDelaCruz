d09150b3c902afd3d88150c9000e10c5c79ca716
BSIT Dela Cruz <cathydelacruz010@gmail.com>
2025-02-12 22:33:31 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+Name = "Cathylene Fate P. Dela Cruz"
+Course = "BSIT"
+Interests_in_programming = "Game Development"
+
+print("Name" + Name)
+print("Course" + Course)
+print("Interests in programming" + Interests_in_programming)
