a7ac4556572c806a611edf3528fc07a746ff7a7c
BSIT Dela Cruz <cathydelacruz010@gmail.com>
2025-02-13 19:24:47 +0800

    My first Python program

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+Name = "Cathylene Fate Dela Cruz"
+Course = "BSIT"
+Interests_in_programming = "Game Development"
+
 print("Hello world!")
 
-print(" Name: " + Name)
-print(" Course: " + Course)
-print(" Interests in programming: " + Interests_in_programming)
+print("Name: " + Name)
+print("Course: " + Course)
+print("Interests in programming: " + Interests_in_programming)
